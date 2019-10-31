92898669ef6feec24a59d48e7a6314e6e4f5c5dc
jobtalle <jobtalle@hotmail.com>
2019-10-31 15:11:24 +0100

    Island is positioned and rendered

--- js/island.js
+++ js/island.js
@@ -1,9 +1,19 @@
-const Island = function(width, height) {
+const Island = function(size) {
     this.update = timeStep => {
 
     };
 
-    this.draw = context => {
+    this.draw = (context, angle) => {
+        context.save();
+        context.scale(1, 0.5);
+        context.rotate(angle);
 
+        context.beginPath();
+        context.moveTo(0, 0);
+        context.arc(0, 0, 64, 0, Math.PI * 2);
+        context.strokeStyle = "red";
+        context.stroke();
+
+        context.restore();
     };
 };
--- js/main.js
+++ js/main.js
@@ -1,19 +1,33 @@
 const TIME_STEP_MAX = 0.1;
+const ANGLE_SPEED = 0.6;
 
 const wrapper = document.getElementById("wrapper");
 const canvas = document.getElementById("renderer");
 let lastDate = new Date();
 let island = null;
+let angle = 0;
 
 const resize = () => {
     canvas.width = wrapper.offsetWidth;
     canvas.height = wrapper.offsetHeight;
-    island = new Island(canvas.width, canvas.height);
+    island = new Island(canvas.width * 0.5);
 };
 
 const update = timeStep => {
+    const context = canvas.getContext("2d");
+
     island.update(Math.min(timeStep, TIME_STEP_MAX));
-    island.draw(canvas.getContext("2d"));
+
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.save();
+    context.translate(canvas.width * 0.5, canvas.height * 0.5);
+
+    if ((angle += timeStep * ANGLE_SPEED) > Math.PI * 2)
+        angle -= Math.PI * 2;
+
+    island.draw(context, angle);
+
+    context.restore();
 };
 
 const loopFunction = () => {
