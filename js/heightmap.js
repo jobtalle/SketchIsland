e4ffdf4c4fc6ef51fed8467e9cca111817c70f3a
Job <jobtalle@hotmail.com>
2019-11-01 00:51:04 +0100

    Islands are round

--- js/heightmap.js
+++ js/heightmap.js
@@ -4,9 +4,16 @@ const Heightmap = function(size) {
 
     const fill = () => {
         const s = 0.05;
+        const m = 1.5;
+        const water = 0.1;
 
-        for (let y = 0; y < size; ++y) for (let x = 0; x < size; ++x)
-            heights[x + size * y] = Math.pow(cubicNoiseSample2(noise, x * s, y * s), 3) - 0.08;
+        for (let y = 0; y < size; ++y) for (let x = 0; x < size; ++x) {
+            const dx = size * 0.5 - x;
+            const dy = size * 0.5 - y;
+            const multiplier = Math.max(0, 1 - Math.sqrt(dx * dx + dy * dy) / size * 2);
+
+            heights[x + size * y] = multiplier * m * Math.pow(cubicNoiseSample2(noise, x * s, y * s), 1.5) - water;
+        }
     };
 
     this.get = (x, y) => heights[x + size * y];
