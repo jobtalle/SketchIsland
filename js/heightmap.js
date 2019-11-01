75e9036b917995ca9c3bf674dc290d922ff8b254
jobtalle <jobtalle@hotmail.com>
2019-11-01 13:43:06 +0100

    Peak falloff is a powered cosine

--- js/heightmap.js
+++ js/heightmap.js
@@ -6,7 +6,8 @@ const Heightmap = function(size) {
         for (let y = 0; y < size; ++y) for (let x = 0; x < size; ++x) {
             const dx = size * 0.5 - x;
             const dy = size * 0.5 - y;
-            const multiplier = Math.max(0, 1 - Math.sqrt(dx * dx + dy * dy) / size * 2);
+            const peakDistance = Math.min(1, Math.sqrt(dx * dx + dy * dy) / size * 2);
+            const multiplier = Heightmap.MULTIPLIER * Math.pow(0.5 + 0.5 * Math.cos(Math.PI * peakDistance), Heightmap.PEAK_POWER);
 
             let sample = 0;
             let influence = Heightmap.OCTAVE_INFLUENCE_INITIAL;
@@ -19,7 +20,7 @@ const Heightmap = function(size) {
                 scale *= Heightmap.SCALE_FALLOFF;
             }
 
-            heights[x + size * y] = multiplier * Heightmap.MULTIPLIER * Math.pow(sample, Heightmap.POWER) - Heightmap.WATER_THRESHOLD;
+            heights[x + size * y] = multiplier * Math.pow(sample, Heightmap.POWER) - Heightmap.WATER_THRESHOLD;
         }
     };
 
@@ -28,13 +29,14 @@ const Heightmap = function(size) {
     fill();
 };
 
-Heightmap.WATER_THRESHOLD = 0.2;
-Heightmap.POWER = 3.5;
-Heightmap.MULTIPLIER = 5;
-Heightmap.SCALE_INITIAL = 0.007;
-Heightmap.SCALE_FALLOFF = 1.7;
+Heightmap.WATER_THRESHOLD = 0.1;
+Heightmap.POWER = 4;
+Heightmap.MULTIPLIER = 7;
+Heightmap.PEAK_POWER = 0.7;
+Heightmap.SCALE_INITIAL = 0.008;
+Heightmap.SCALE_FALLOFF = 1.75;
 Heightmap.OCTAVES = 6;
-Heightmap.OCTAVE_FALLOFF = 1.3;
+Heightmap.OCTAVE_FALLOFF = 1.5;
 Heightmap.OCTAVE_INFLUENCE_INITIAL = ((Heightmap.OCTAVE_FALLOFF - 1) *
     (Math.pow(Heightmap.OCTAVE_FALLOFF, Heightmap.OCTAVES))) /
     (Math.pow(Heightmap.OCTAVE_FALLOFF, Heightmap.OCTAVES) - 1) / Heightmap.OCTAVE_FALLOFF;
