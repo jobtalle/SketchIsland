8600fa85d77cd24229cf490c611dd9dd3bc76521
Job <jobtalle@hotmail.com>
2019-11-06 18:35:21 +0100

    Village adapts to scale

--- js/plan/village.js
+++ js/plan/village.js
@@ -17,11 +17,10 @@ const Village = function(height, heightmap, bounds, scale) {
 
         const candidates = [];
 
-        for (let y = bounds.start.y; y < bounds.end.y; y += Village.PROBE_STRIDE) {
-            for (let x = bounds.start.x; x < bounds.end.x; x += Village.PROBE_STRIDE) {
+        for (let y = bounds.start.y; y < bounds.end.y; y += Village.PROBE_STRIDE * scale) {
+            for (let x = bounds.start.x; x < bounds.end.x; x += Village.PROBE_STRIDE * scale) {
                 const xr = Math.round(x);
                 const yr = Math.round(y);
-                const h = heightmap.getHeight(xr, yr);
 
                 if (!suitable(xr, yr))
                     continue;
@@ -38,47 +37,22 @@ const Village = function(height, heightmap, bounds, scale) {
         return candidates[0];
     };
 
-    const placeHut = (shapes, at) => {
-        const base = new ShapeCylinder(
-            at.copy().subtract(new Vector3(0, 0, Village.HUT_DEPTH)),
-            Village.HUT_RADIUS * Village.HUT_ROOF_RADIUS,
-            Village.HUT_DEPTH,
-            Village.COLOR_HUT_BASE);
-        const walls = new ShapeCone(
-            at.copy().add(new Vector3(0, 0, Village.HUT_HEIGHT)),
-            Village.HUT_RADIUS * Village.HUT_ROOF_RADIUS,
-            Village.HUT_HEIGHT * Village.HUT_ROOF_HEIGHT,
-            Village.COLOR_HUT_ROOF);
-        const roof = new ShapeCylinder(
-            at,
-            Village.HUT_RADIUS,
-            Village.HUT_HEIGHT,
-            Village.COLOR_HUT_WALLS);
-
-        shapes.cropBounds(walls.bounds);
-        shapes.clear(walls.bounds);
-
-        shapes.add(walls);
-        shapes.add(base);
-        shapes.add(roof);
-    };
-
     this.place = shapes => {
         const HutPlan = function(location) {
             this.base = new ShapeCylinder(
-                location.copy().subtract(new Vector3(0, 0, Village.HUT_DEPTH)),
-                Village.HUT_RADIUS * Village.HUT_ROOF_RADIUS,
-                Village.HUT_DEPTH,
+                location.copy().subtract(new Vector3(0, 0, Village.HUT_DEPTH * scale)),
+                Village.HUT_RADIUS * Village.HUT_ROOF_RADIUS * scale,
+                Village.HUT_DEPTH * scale,
                 Village.COLOR_HUT_BASE);
-            this.walls = new ShapeCone(
-                location.copy().add(new Vector3(0, 0, Village.HUT_HEIGHT)),
-                Village.HUT_RADIUS * Village.HUT_ROOF_RADIUS,
-                Village.HUT_HEIGHT * Village.HUT_ROOF_HEIGHT,
+            this.roof = new ShapeCone(
+                location.copy().add(new Vector3(0, 0, Village.HUT_HEIGHT * scale)),
+                Village.HUT_RADIUS * Village.HUT_ROOF_RADIUS * scale,
+                Village.HUT_HEIGHT * Village.HUT_ROOF_HEIGHT * scale,
                 Village.COLOR_HUT_ROOF);
-            this.roof = new ShapeCylinder(
+            this.walls = new ShapeCylinder(
                 location.copy(),
-                Village.HUT_RADIUS,
-                Village.HUT_HEIGHT,
+                Village.HUT_RADIUS * scale,
+                Village.HUT_HEIGHT * scale,
                 Village.COLOR_HUT_WALLS);
         };
 
@@ -90,8 +64,8 @@ const Village = function(height, heightmap, bounds, scale) {
 
         plans.push(new HutPlan(new Vector3(origin.x, origin.y, heightmap.getHeight(origin.x, origin.y) * height)));
 
-        let radius = Village.HUT_SPACING;
-        let huts = Math.floor(Math.PI * radius * 2 / Village.HUT_SPACING);
+        let radius = Village.HUT_SPACING * scale;
+        let huts = Math.floor(Math.PI * radius * 2 / Village.HUT_SPACING * scale);
 
         for (let i = 0; i < huts; ++i) {
             const angle = Math.PI * 2 * i / huts;
@@ -113,9 +87,9 @@ const Village = function(height, heightmap, bounds, scale) {
         }
 
         for (const plan of plans) {
+            shapes.add(plan.roof);
             shapes.add(plan.walls);
             shapes.add(plan.base);
-            shapes.add(plan.roof);
         }
     };
 };
@@ -123,13 +97,13 @@ const Village = function(height, heightmap, bounds, scale) {
 Village.HEIGHT_MIN = 0.15;
 Village.HEIGHT_MAX = 0.35;
 Village.DOT_MIN = 0.7;
-Village.HUT_RADIUS = 5;
-Village.HUT_HEIGHT = 6;
-Village.HUT_DEPTH = 8;
+Village.HUT_RADIUS = 10;
+Village.HUT_HEIGHT = 12;
+Village.HUT_DEPTH = 6;
 Village.HUT_ROOF_RADIUS = 1.25;
 Village.HUT_ROOF_HEIGHT = 0.8;
 Village.HUT_SPACING = Village.HUT_RADIUS * 2.5;
-Village.MIN_SIZE = 3;
+Village.MIN_SIZE = 1;
 Village.PROBE_STRIDE = 24;
 Village.COLOR_HUT_BASE = StyleUtils.getColor("--color-hut-base");
 Village.COLOR_HUT_WALLS = StyleUtils.getColor("--color-hut-walls");
